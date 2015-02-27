05846ca88d30d2e90e2f0520b712927a620fa5fa
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2015-02-28 00:44:16 +0100

    Make sure app runs on heroku

--- index.js
+++ index.js
@@ -29,35 +29,35 @@ app.get('/', function (req, res) {
 
     if (!cat) {
         res.send(
-            '<p>Use <strong>?cat=</strong><em>{Name of Wikimedia Commons category}</em> (e.g. Category:CH-BAR_Collection_First_World_War_Switzerland) to download a CSV of all the categories for each file in that category.</p>'
+            '<p>Use <strong>?cat=<em>{Name of Wikimedia Commons category}</em></strong> (e.g. Category:CH-BAR_Collection_First_World_War_Switzerland) to download a CSV of all the categories for each file in that category.</p>'
         );
-    } else {
-        res.attachment('catexport_' + cat + '.csv');
-        res.write('filename,category');
-        getResultPage(
-            {
-                'category': cat,
-                'continue_obj': {
-                    'continue': ''
-                }
-            },
-            function(response) {
-                _.each(response.query.pages, function(page) {
-                    res.write("\n");
-                    _.each(page.categories, function(category) {
-                        console.log('\"' + page.title + '\",\"' + category.title + '\"');
-                        res.write(page.title + ',' + category.title);
-                    });
-                });
-            },
-            function() {
-                res.end();
-            }
-        );
+        return;
     }
+    res.attachment('catexport_' + cat + '.csv');
+    res.write('filename,category');
+    getResultPage(
+        {
+            'category': cat,
+            'continue_obj': {
+                'continue': ''
+            }
+        },
+        function(response) {
+            _.each(response.query.pages, function(page) {
+                _.each(page.categories, function(category) {
+                    console.log('\n\"' + page.title + '\",\"' + category.title + '\"');
+                    res.write(page.title + ',' + category.title);
+                });
+            });
+        },
+        function() {
+            res.end();
+        }
+    );
 })
 
-var server = app.listen(7777, function () {
+var port = process.env.PORT || 7777;
+var server = app.listen(port, function () {
     var host = server.address().address
     var port = server.address().port
 
