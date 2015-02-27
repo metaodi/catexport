7269dc1f48ebc7a2c09feb0f447c17392b105e7b
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2015-02-27 19:58:43 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+# https://commons.wikimedia.org/w/api.php?action=query&generator=categorymembers&continue=&gcmtype=file&gcmlimit=10&cllimit=max&gcmtitle=Category:CH-BAR_Collection_First_World_War_Switzerland&prop=categories
+
+
