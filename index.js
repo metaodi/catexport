32fa32e5cdb312302d5e2e15cba324dac4881211
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2015-02-28 06:41:30 +0100

    Fixed default text

--- index.js
+++ index.js
@@ -91,5 +91,5 @@ var server = app.listen(port, function () {
     var host = server.address().address
     var port = server.address().port
 
-    console.log('Example app listening at http://%s:%s', host, port)
+    console.log('catexport app listening at http://%s:%s', host, port)
 })
